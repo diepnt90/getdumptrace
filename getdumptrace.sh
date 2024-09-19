96635bb91c6aac911abbcbe8ac0ce65dcfb7641f
Trần Quang Huy <diep90.nt@gmail.com>
2024-09-19 15:42:38 +0700

    Update getdumptrace.sh

--- getdumptrace.sh
+++ getdumptrace.sh
@@ -1,16 +1,2 @@
 #!/bin/bash
-
-# Find the PID of the .NET process
-pid=$(/tools/dotnet-dump ps | grep '/usr/share/dotnet/dotnet' | awk '{print $1}')
-
-# Check if PID was found
-if [ -z "$pid" ]; then
-    echo "Error: Could not find the .NET process."
-    exit 1
-fi
-
-# Get the environment variables of the process
-environ=$(cat "/proc/$pid/environ" | tr '\0' '\n')
-
-# Display the environment variables
-echo "$environ"
+echo "Hello"
